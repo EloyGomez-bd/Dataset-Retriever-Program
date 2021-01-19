d8e229f120b79a64ed0109ae659b40fcbcd02ab3
EloyGomez <emoreno@outlook.dk>
2021-01-19 23:51:37 +0100

    acquisition module done. argparse done

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+import argparse
+from p_acquisition.m_acquisition import data_collection
+
+
+def argument_parser():
+    """
+    parse arguments to script
+    """
+
+    parser = argparse.ArgumentParser()
+
+    # Arguments here
+    parser.add_argument("-p", "--path", help="specify database location", type=str, required=True)
+
+    # Arguments here
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main(arguments):
+
+    print('Starting process...')
+
+    path = arguments.path
+
+    full_df = data_collection(path=path)
+
+    print('Importing data to directory...')
+
+    full_df.to_csv('./data/raw/full_data.csv', index=False)
+
+    print('Finished process...')
+
+
+
 if __name__ == '__main__':
+
+    arguments = argument_parser()
+
+    main(arguments)
--- p_acquisition/m_acquisition.py
+++ p_acquisition/m_acquisition.py
@@ -1,3 +1,41 @@
-from sqlalchemy import create_engine
 import pandas as pd
+from sqlalchemy import create_engine
+
+
+def connection_to_db(path):
+
+    print(f'Establishing connection to database from {path}')
+
+    engine = create_engine(f'sqlite:///{path}')
+
+    print('Connexion to the database has been a success.')
+
+    return engine
+
+
+def data_collection(path):
+
+    # SQL Query to get all data from database
+
+    query = """
+            SELECT * FROM country_info
+            LEFT JOIN personal_info ON country_info.uuid = personal_info.uuid
+            LEFT JOIN career_info ON country_info.uuid = career_info.uuid
+            LEFT JOIN poll_info ON country_info.uuid = poll_info.uuid
+            """
+
+    engine = connection_to_db(path)
+
+    read_query = pd.read_sql_query(query, engine)
+
+    full_data_frame = pd.DataFrame(read_query)
+
+    # With query above we obtain a data frame with several columns (uuid) duplicated. We want just one..
+
+    data_frame: object = full_data_frame.loc[:, ~full_data_frame.columns.duplicated()]
+
+    return data_frame
+
+
+
 
